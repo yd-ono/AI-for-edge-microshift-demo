aecee066c836672d5e20bb51ecc6f3ca06a8492a
Robert Bohne <robert.bohne@redhat.com>
2022-12-30 14:15:03 +0000

    Fixed push

--- webapp/build-cpu-only.sh
+++ webapp/build-cpu-only.sh
@@ -4,4 +4,5 @@ podman build \
   -t quay.io/rbohne/ai-for-edge-microshift-demo:webapp-cpu-only-$(uname -p) \
   -f Containerfile.cpu-only .
 
-podman push quay.io/rbohne/ai-for-edge-microshift-demo:cpu-only-$(uname -p)
+podman push \
+     quay.io/rbohne/ai-for-edge-microshift-demo:webapp-cpu-only-$(uname -p)

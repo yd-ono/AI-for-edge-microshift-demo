306598ea56fa67675f4eabec7018a9c26d1292fa
Max Murakami <mmurakam@redhat.com>
2023-01-27 17:58:00 +0000

    fixed model training pipeline

--- model-training-pipeline/model_upload.py
+++ model-training-pipeline/model_upload.py
@@ -1,27 +1,17 @@
 from datetime import datetime
-from os import environ
 
 from boto3 import client
 
-
-print('Reading environment variables.')
-
-s3_access_key = environ.get('AWS_ACCESS_KEY_ID')
-s3_secret_key = environ.get('AWS_SECRET_ACCESS_KEY')
-s3_endpoint_url = environ.get('AWS_S3_ENDPOINT')
-s3_bucket_name = environ.get('AWS_S3_BUCKET')
-
-print(f'S3 access key: {s3_access_key}')
-print(f'S3 secret key: {s3_secret_key}')
-print(f'S3 endpoint URL: {s3_endpoint_url}')
-print(f'S3 bucket name: {s3_bucket_name}')
+import s3_config
 
 
 def instantiate_s3_client():
     print('Instantiating S3 client.')
     s3_client = client(
-        's3', endpoint_url=s3_endpoint_url,
-        aws_access_key_id=s3_access_key, aws_secret_access_key=s3_secret_key
+        's3',
+        endpoint_url=s3_config.endpoint_url,
+        aws_access_key_id=s3_config.access_key,
+        aws_secret_access_key=s3_config.secret_key
     )
     return s3_client
 
@@ -35,10 +25,10 @@ def get_target_object_name():
 
 def upload_model_package(s3_client, target_object_name):
     print(f'Uploading model package to {target_object_name} '
-          f'in bucket {s3_bucket_name}.')
+          f'in bucket {s3_config.bucket_name}.')
     with open('model.data', 'rb') as model_package_file:
         s3_client.upload_fileobj(
-            model_package_file, s3_bucket_name, target_object_name
+            model_package_file, s3_config.bucket_name, target_object_name
         )
 
 

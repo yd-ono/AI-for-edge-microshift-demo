1cc8971b94c3923d7aeca6f711a90b5ea5c3cbaf
knative-impress <pinkink.yudaiono@gmail.com>
2023-07-03 17:22:14 +0900

    commit

--- container-images/cpu-only/apply-manifest-list.sh
+++ container-images/cpu-only/apply-manifest-list.sh
@@ -5,15 +5,15 @@ buildah manifest create ai-for-edge-microshift-demo-cpu-only
 buildah manifest add \
     --arch amd64 \
     ai-for-edge-microshift-demo-cpu-only \
-    docker://quay.io/rbohne/ai-for-edge-microshift-demo:cpu-only-x86_64
+    docker://quay.io/yono/ai-for-edge-microshift-demo:cpu-only-x86_64
 
 buildah manifest add \
     --arch arm64 \
     ai-for-edge-microshift-demo-cpu-only \
-    docker://quay.io/rbohne/ai-for-edge-microshift-demo:cpu-only-aarch64
+    docker://quay.io/yono/ai-for-edge-microshift-demo:cpu-only-aarch64
 
 buildah manifest push \
     ai-for-edge-microshift-demo-cpu-only \
-    docker://quay.io/rbohne/ai-for-edge-microshift-demo:cpu-only
+    docker://quay.io/yono/ai-for-edge-microshift-demo:cpu-only
 
 buildah manifest rm ai-for-edge-microshift-demo-cpu-only
--- container-images/cpu-only/build-and-push.sh
+++ container-images/cpu-only/build-and-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 podman build \
-  -t quay.io/rbohne/ai-for-edge-microshift-demo:cpu-only-$(uname -p) \
+  -t quay.io/yono/ai-for-edge-microshift-demo:cpu-only-$(uname -p) \
   -f Containerfile .
 
-podman push quay.io/rbohne/ai-for-edge-microshift-demo:cpu-only-$(uname -p)
+podman push quay.io/yono/ai-for-edge-microshift-demo:cpu-only-$(uname -p)
